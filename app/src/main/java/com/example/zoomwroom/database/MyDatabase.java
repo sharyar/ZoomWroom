7f04d493edd654f879d6d9344b33d473d108626a
SiyuanLiu <lsy997168748@gmail.com>
2020-03-10 15:23:53 +0800

    add database

---
+++ app/src/main/java/com/example/zoomwroom/database/MyDatabase.java
@@ -0,0 +1,236 @@
+package com.example.zoomwroom.database;
+
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+
+import com.example.zoomwroom.Entities.DriveRequest;
+import com.example.zoomwroom.Entities.Driver;
+import com.example.zoomwroom.Entities.Rider;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+import com.google.firebase.firestore.QuerySnapshot;
+
+import java.util.ArrayList;
+
+public class MyDatabase {
+    private static FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+    public MyDatabase(){ }
+
+    /**
+     * This function adds the request to data base
+     * @param driveRequest  a request object
+     * @return requestID return the id of request
+     */
+    public static String addRequest(DriveRequest driveRequest) {
+        final CollectionReference collectionReference = db.collection("DriverRequest");
+        String requestID = collectionReference.document().getId();
+        driveRequest.setRequestID(requestID);
+
+        collectionReference
+                .document(requestID)
+                .set(driveRequest)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Driver request", "data storing is successful");
+                    }
+
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Driver request", "data addition failed");
+                    }
+                });
+        return requestID;
+    }
+
+    /**
+     * This function get an array of requests
+     * @param driverID Driver's ID
+     * @return driveRequests an array of drive requests related to this driver
+     */
+    public static ArrayList<DriveRequest> getDriverRequest(String driverID){
+        final CollectionReference collectionReference = db.collection("DriverRequest");
+        ArrayList<DriveRequest> driveRequests = new ArrayList<>();
+
+        Task<QuerySnapshot> task = collectionReference
+                .whereEqualTo("driverID", driverID)
+                .get();
+        while (!task.isSuccessful()) {}
+        for (QueryDocumentSnapshot doc : task.getResult()) {
+            DriveRequest request = doc.toObject(DriveRequest.class);
+            driveRequests.add(request);
+        }
+
+        return driveRequests;
+    }
+
+    /**
+     * This function adds the request to data base
+     * @param driveRequest a request object
+     * @return requestID return the id of request
+     */
+    public static void updateRequest(DriveRequest driveRequest) {
+        final CollectionReference collectionReference = db.collection("DriverRequest");
+        String requestID = driveRequest.getRequestID();
+        collectionReference.document(requestID).set(driveRequest);
+    }
+
+    /**
+     * This function is to update a rider
+     * @param rider
+     */
+    public static void updateRider(final Rider rider){
+        final CollectionReference collectionReference = db.collection("Riders");
+        collectionReference.whereEqualTo("userID", rider.getUserID()).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                    String id = doc.getId();
+                    collectionReference.document(id).set(rider);
+                }
+            }
+        });
+    }
+
+    /**
+     * similar to updateRider
+     * @param driver
+     */
+    public static void updateDriver(final Driver driver){
+        final CollectionReference collectionReference = db.collection("Drivers");
+        collectionReference.whereEqualTo("userID", driver.getUserID()).get().addOnSuccessListener(new OnSuccessListener<QuerySnapshot>() {
+            @Override
+            public void onSuccess(QuerySnapshot queryDocumentSnapshots) {
+                for (QueryDocumentSnapshot doc : queryDocumentSnapshots) {
+                    String id = doc.getId();
+                    collectionReference.document(id).set(driver);
+                }
+            }
+        });
+    }
+
+    /**
+     * similar to getDriverRequest
+     * @param riderID
+     */
+    public static ArrayList<DriveRequest> getRiderRequest(String riderID){
+        final CollectionReference collectionReference = db.collection("DriverRequest");
+        ArrayList<DriveRequest> driveRequests = new ArrayList<DriveRequest>();
+
+        Task<QuerySnapshot> task = collectionReference
+                .whereEqualTo("riderID", riderID)
+                .get();
+        while (!task.isSuccessful()) {}
+        for (QueryDocumentSnapshot doc : task.getResult()) {
+            DriveRequest request = doc.toObject(DriveRequest.class);
+            driveRequests.add(request);
+        }
+        return driveRequests;
+    }
+
+    /**
+     * get target object rider
+     * @param userID
+     */
+    public static Rider getRider(String userID) {
+        final CollectionReference collectionReference = db.collection("Riders");
+        Task<QuerySnapshot> task = collectionReference
+                .whereEqualTo("userID", userID)
+                .get();
+        while (!task.isSuccessful()) {}
+        if (task.getResult().toObjects(Rider.class).size() == 0){
+            Log.d("Rider", "no user is found");
+            return null;
+        }
+        if (task.getResult().toObjects(Rider.class).size() > 1){
+            Log.d("Rider", "userID is not unique");
+            return null;
+        }
+        Rider rider = task.getResult().toObjects(Rider.class).get(0);
+        return rider;
+    }
+
+    /**
+     * add the rider to the database
+     * @param rider
+     */
+    public static void addRider(Rider rider) {
+        final CollectionReference collectionReference = db.collection("Riders");
+
+        collectionReference
+                .document()
+                .set(rider)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Rider", "data storing is successful");
+                    }
+
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Rider", "data addition failed");
+                    }
+                });
+    }
+
+    /**
+     * add the driver to database
+     * @param driver
+     */
+    public static void addDriver(Driver driver) {
+        final CollectionReference collectionReference = db.collection("Drivers");
+
+        collectionReference
+                .document()
+                .set(driver)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void aVoid) {
+                        Log.d("Driver", "data storing is successful");
+                    }
+
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Log.d("Driver", "data addition failed");
+                    }
+                });
+    }
+
+    /**
+     * get the target object driver
+     * @param userID
+     */
+    public static Driver getDriver(String userID) {
+        final CollectionReference collectionReference = db.collection("Drivers");
+        Task<QuerySnapshot> task = collectionReference
+                .whereEqualTo("userID", userID)
+                .get();
+        while (!task.isSuccessful()) {}
+        if (task.getResult().toObjects(Driver.class).size() == 0){
+            Log.d("Driver", "no user is found");
+            return null;
+        }
+        if (task.getResult().toObjects(Driver.class).size() > 1){
+            Log.d("Driver", "userID is not unique");
+            return null;
+        }
+        Driver driver = task.getResult().toObjects(Driver.class).get(0);
+        return driver;
+    }
+
+}
+
+

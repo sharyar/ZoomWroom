2789a28d3fda80e7cc50d96c7db5ae04a96eabef
Sharyar Memon <sharyar@ualberta.ca>
2020-03-10 22:45:44 -0600

    feat: implements required fields for editUserActivity

--- app/src/main/java/com/example/zoomwroom/EditUserProfileActivity.java
+++ app/src/main/java/com/example/zoomwroom/EditUserProfileActivity.java
@@ -3,6 +3,9 @@ package com.example.zoomwroom;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+
+import com.google.android.material.textfield.TextInputEditText;
 
 public class EditUserProfileActivity extends AppCompatActivity {
 
@@ -12,6 +15,15 @@ public class EditUserProfileActivity extends AppCompatActivity {
         setContentView(R.layout.activity_edit_user_profile);
         ActionBar actionBar = getSupportActionBar();
         actionBar.setTitle("Edit Profile");
+
+
+        // Text Fields used within the activity
+        TextInputEditText firstNameEditText = findViewById(R.id.edit_user_first_name_editText);
+        TextInputEditText lastNameEditText = findViewById(R.id.edit_user_last_name_editText);
+        TextInputEditText userNameEditText = findViewById(R.id.edit_user_name_editText);
+        TextInputEditText phoneNumberEditText = findViewById(R.id.edit_user_phone_editText);
+        Button confirmChangesBtn = findViewById(R.id.edit_user_profile_confirm_btn);
+
 
     }
 }

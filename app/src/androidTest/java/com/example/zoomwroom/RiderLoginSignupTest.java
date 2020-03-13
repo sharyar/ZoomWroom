e80b3b6e6e68aae74b631b93606650c26724d06f
Amanda Nguyen <amandanguyen@Amandas-MacBook-Pro.local>
2020-03-12 21:17:50 -0600

    added rules dependencies

--- app/src/androidTest/java/com/example/zoomwroom/RiderLoginSignupTest.java
+++ app/src/androidTest/java/com/example/zoomwroom/RiderLoginSignupTest.java
@@ -1,4 +1,25 @@
 package com.example.zoomwroom;
 
+import androidx.test.platform.app.InstrumentationRegistry;
+
+import com.robotium.solo.Solo;
+
+import org.junit.Before;
+import org.junit.Rule;
+
 public class RiderLoginSignupTest {
+    private Solo solo;
+    @Rule
+    public ActivityTestRule<MainActivity> rule =
+            new ActivityTestRule<>(MainActivity.class, true, true);
+
+    /**
+     * Runs before all tests and creates solo instance
+     * @throws Exception
+     * */
+    @Before
+    public void setUp() throws Exception{
+        solo = new Solo(InstrumentationRegistry.getInstrumentation(),rule.getActivity());
+    }
+
 }

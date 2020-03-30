02a17b7c99678d8bc65a8bc0413334e17e6c95ca
SiyuanLiu <lsy997168748@gmail.com>
2020-03-30 14:00:59 +0800

    feat: current request

--- app/src/main/java/com/example/zoomwroom/DriverHomeActivity.java
+++ app/src/main/java/com/example/zoomwroom/DriverHomeActivity.java
@@ -49,6 +49,7 @@ public class DriverHomeActivity extends MapsActivity implements GoogleMap.OnMark
 
     FloatingActionButton profileBtn; // Used to open the user's profile
     FloatingActionButton driveRequestListBtn;
+    FloatingActionButton currentRequest;
 
 
     private String driverID;
@@ -108,6 +109,23 @@ public class DriverHomeActivity extends MapsActivity implements GoogleMap.OnMark
             public void onClick(View v) {
                 Intent myIntent = new Intent(v.getContext(), AcceptedDriveRequestsActivity.class);
                 startActivityForResult(myIntent, 0);
+            }
+        });
+
+        // Current request button
+        currentRequest = findViewById(R.id.current_request);
+
+        currentRequest.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Bundle b = new Bundle();
+                b.putString("userID", driverID);
+
+                FragmentManager fragmentManager = getSupportFragmentManager();
+                final FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+                final FragmentDriverCurrentRequest currentRequestFragment = new FragmentDriverCurrentRequest();
+                currentRequestFragment.setArguments(b);
+                currentRequestFragment.show(getSupportFragmentManager(), currentRequestFragment.getTag());
             }
         });
 

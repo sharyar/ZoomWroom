6263c35e33a7e1ae1dc183c65228fe715b351d6a
Sharyar Memon <sharyar@ualberta.ca>
2020-03-30 15:09:17 -0600

    feat: implements user input validation for driver signup
    Implements new entity class UserDataValidation that has static functions for ensuring that the data is input correctly when user is signing up or changing their info.

--- app/src/main/java/com/example/zoomwroom/DriverSignUpActivity.java
+++ app/src/main/java/com/example/zoomwroom/DriverSignUpActivity.java
@@ -1,21 +1,20 @@
 //sources: https://firebase.google.com/docs/auth/android/password-auth#create_a_password-based_account
 
 package com.example.zoomwroom;
-
 import android.content.Intent;
 import android.os.Bundle;
+import android.telephony.PhoneNumberUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ProgressBar;
 import android.widget.Toast;
-
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
-
 import com.example.zoomwroom.Entities.ContactInformation;
 import com.example.zoomwroom.Entities.Driver;
+import com.example.zoomwroom.Entities.UserDataValidation;
 import com.example.zoomwroom.database.MyDataBase;
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
@@ -23,6 +22,8 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseAuthUserCollisionException;
+import com.google.firebase.auth.FirebaseAuthWeakPasswordException;
 import com.google.firebase.auth.FirebaseUser;
 import com.google.firebase.firestore.FirebaseFirestore;
 
@@ -30,12 +31,29 @@ public class DriverSignUpActivity extends AppCompatActivity {
     // You declare this in every activity that needs access to the database
     FirebaseFirestore database = FirebaseFirestore.getInstance();
 
-    //Used for user authentication and signup
+    //Used for user authentication and sign up
     private FirebaseAuth mAuth;
 
     private static final String TAG = "EmailPassword";
 
+    // Declare variables required for activity
+    EditText firstNameEditText;
+    EditText lastNameEditText;
+    EditText emailAddressEditText;
+    EditText passWordEditText;
+    EditText userNameEditText;
+    EditText phoneNumberEditText;
+    
+    
+    String email;
+    String passWord;
+    String firstNameText;
+    String lastNameText; 
+    String userName;
+    String phoneNumber;
 
+    Button signUpDriver;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -46,36 +64,34 @@ public class DriverSignUpActivity extends AppCompatActivity {
 
         ProgressBar bar = findViewById(R.id.driver_signup_progress_bar);
         bar.setVisibility(View.INVISIBLE);
-
-        EditText firstNameEditText = findViewById(R.id.driverSignupFName);
-        EditText lastNameEditText = findViewById(R.id.driverSignupLName);
-        EditText emailAddressEditText = findViewById(R.id.driverSignupEmailAddress);
-        EditText passWordEditText = findViewById(R.id.driverSignupPassWord);
-        EditText userNameEditText = findViewById(R.id.driverSignupUserName);
-        EditText phoneNumberEditText = findViewById(R.id.driverSignupPhoneNumber);
-        Button signUpDriver = findViewById(R.id.driverSignupSignupBtn);
+        
+        // Set fields to views
+        firstNameEditText = findViewById(R.id.driverSignupFName);
+        lastNameEditText = findViewById(R.id.driverSignupLName);
+        emailAddressEditText = findViewById(R.id.driverSignupEmailAddress);
+        passWordEditText = findViewById(R.id.driverSignupPassWord);
+        userNameEditText = findViewById(R.id.driverSignupUserName);
+        phoneNumberEditText = findViewById(R.id.driverSignupPhoneNumber);
+        
+        // Set Button to view
+        signUpDriver = findViewById(R.id.driverSignupSignupBtn);
 
 
         //Uses the email address and password fields to create a new user within the database.
         signUpDriver.setOnClickListener((View v) -> {
             bar.setVisibility(View.VISIBLE);
-            String email = emailAddressEditText.getText().toString().trim();
-            String passWord = passWordEditText.getText().toString();
-            String firstNameText = firstNameEditText.getText().toString().trim();
-            String lastNameText = lastNameEditText.getText().toString().trim();
-            String userName = userNameEditText.getText().toString().trim();
-            String phoneNumber = phoneNumberEditText.getText().toString().trim();
+            email = emailAddressEditText.getText().toString().trim();
+            passWord = passWordEditText.getText().toString();
+            firstNameText = firstNameEditText.getText().toString().trim();
+            lastNameText = lastNameEditText.getText().toString().trim();
+            userName = userNameEditText.getText().toString().trim();
+            phoneNumber = PhoneNumberUtils.normalizeNumber(phoneNumberEditText.getText().toString().trim());
+            
+            
+            // add validation check here:
+            
 
-            if (email.isEmpty() || passWord.isEmpty() || firstNameText.isEmpty() ||
-                    lastNameText.isEmpty() || userName.isEmpty() || phoneNumber.isEmpty()) {
-                Toast.makeText(DriverSignUpActivity.this, "Please ensure you have " +
-                        "filled out all the fields.", Toast.LENGTH_SHORT).show();
-
-
-            } else if (!MyDataBase.isUserNameUnique(userName)) {
-                Toast.makeText(this, "This username is already taken, please use a new one", Toast.LENGTH_SHORT).show();
-            } else {
-
+            if (areFieldsValid()) {
                 mAuth.createUserWithEmailAndPassword(email, passWord)
                         .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
                             @Override
@@ -109,21 +125,37 @@ public class DriverSignUpActivity extends AppCompatActivity {
                                                 }
                                             });
 
-
-                                    // Open the driver's profile after they signup - temporary
+                                    // opens the driver home page
                                     openDriverHomeActivity();
 
-
                                 } else {
+
+                                    bar.setVisibility(View.INVISIBLE);
+
                                     Log.w(TAG, "createUserWithEmail:failure", task.getException());
-                                    Toast.makeText(DriverSignUpActivity.this, "Sign up Failed, please check your fields",
+
+                                    if (task.getException() instanceof FirebaseAuthWeakPasswordException) {
+                                        Toast.makeText(DriverSignUpActivity.this,
+                                                "Please use a stronger password", Toast.LENGTH_SHORT).show();
+
+                                    } else if (task.getException() instanceof FirebaseAuthUserCollisionException) {
+                                        Toast.makeText(DriverSignUpActivity.this,
+                                                "This email is already registered, please login or contact support.",
+                                                Toast.LENGTH_SHORT).show();
+                                    }
+
+                                    Toast.makeText(DriverSignUpActivity.this, "Sign up " +
+                                                    "Failed, please ensure you are using a correct email address.",
                                             Toast.LENGTH_SHORT).show();
                                 }
                             }
-                        });
+                    });
+
+                // Disables loading bar
+            } else {
+                bar.setVisibility(View.INVISIBLE);
             }
         });
-
 
         Button BackBtn = findViewById(R.id.driverSignupBackBtn);
         BackBtn.setOnClickListener(new View.OnClickListener() {
@@ -133,19 +165,8 @@ public class DriverSignUpActivity extends AppCompatActivity {
             }
         });
 
-//        // Creating another hashmap value
-//        HashMap<String, String> data = new HashMap<>();
-//        data.put("2", "Amy");
-//
-//        // NOTE: Obviously collection path will be drivers but I wanted to test to see if I could add to riders
-//        // in a different activity!
-//        final CollectionReference collectionReference = database.collection("Riders");
-//        collectionReference
-//                .document("2") // name
-//                .set(data);
-//
-
     }
+
     public void OpenDriverModeActivity(){
         Intent intent = new Intent(this,DriverModeActivity.class);
         startActivity(intent);
@@ -154,5 +175,47 @@ public class DriverSignUpActivity extends AppCompatActivity {
     public void openDriverHomeActivity() {
         Intent intent = new Intent(this, DriverHomeActivity.class);
         startActivity(intent);
+    }
+
+    /**
+     * Checks if the user's input is valid based on preset conditions. 
+     * 
+     * @return      boolean value indicating if the fields input by the user are valid or not
+     */
+    boolean areFieldsValid() {
+        if (email.isEmpty() || passWord.isEmpty() || firstNameText.isEmpty() ||
+                lastNameText.isEmpty() || userName.isEmpty() || phoneNumber.isEmpty()) {
+            Toast.makeText(DriverSignUpActivity.this, "Please ensure you have " +
+                    "filled out all the fields.", Toast.LENGTH_SHORT).show();
+
+            return false;
+
+        } else if (!UserDataValidation.isEmailValid(email)) {
+            Toast.makeText(this, "Email address is invalid. Please input a valid email", Toast.LENGTH_SHORT).show();
+
+            return false;
+        } else if (!UserDataValidation.isAlpha(firstNameText) || !UserDataValidation.isAlpha(lastNameText)) {
+            Toast.makeText(this, "First & Last Name can only be alphabetic.",
+                    Toast.LENGTH_SHORT).show();
+
+            return false;
+
+        } else if (!UserDataValidation.isAlphaNumeric(userName)) {
+            Toast.makeText(this, "Username must be a combination of numbers and letters."
+                    , Toast.LENGTH_SHORT).show();
+
+            return false;
+        } else if (!MyDataBase.isUserNameUnique(userName)) {
+            Toast.makeText(this, "This username is already taken, please use a different one",
+                    Toast.LENGTH_SHORT).show();
+
+            return false;
+        } else if (!UserDataValidation.isPhoneNumberValid(phoneNumber)) {
+            Toast.makeText(this, "Please enter a valid phone number." , Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        else {
+            return true;
+        }
     }
 }
---
+++ app/src/main/java/com/example/zoomwroom/Entities/UserDataValidation.java
@@ -0,0 +1,37 @@
+package com.example.zoomwroom.Entities;
+import android.telephony.PhoneNumberUtils;
+import android.util.Patterns;
+
+import java.util.regex.Pattern;
+
+/**
+ * This class implements static methods used for data validation checks to ensure that the user
+ * is inputting correct info when they are registering or editing their profile
+ *
+ * Source: https://www.techiedelight.com/check-string-contains-alphanumeric-characters-java/
+ */
+public class UserDataValidation {
+
+    public static boolean isAlphaNumeric(String userName) {
+        Pattern p = Pattern.compile("^[a-zA-Z0-9]+$");
+
+        return p.matcher(userName).find();
+    }
+
+    public static boolean isPhoneNumberValid(String number) {
+        if (number.length() < 10) {
+            return false;
+        }
+        return Patterns.PHONE.matcher(number).matches();
+    }
+
+    public static boolean isAlpha(String name) {
+        Pattern p = Pattern.compile("^[a-zA-Z]");
+        return p.matcher(name).find();
+    }
+
+    public static boolean isEmailValid(String email) {
+        return Patterns.EMAIL_ADDRESS.matcher(email).matches();
+    }
+
+}
